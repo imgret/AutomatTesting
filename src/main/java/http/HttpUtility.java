072d4a817dbf97760ace0b7c389439d93b7b32e2
Imre Gretsi <imgret@ttu.ee>
2017-09-25 00:30:22 +0300

    add files with tests

---
+++ src/main/java/http/HttpUtility.java
@@ -0,0 +1,12 @@
+package http;
+
+public class HttpUtility {
+
+    public static String makeHttpGetRequest(String requestUrl){
+        return "";
+    }
+
+    public static boolean downloadWeatherForecastFile(String location){
+        return false;
+    }
+}
---
+++ src/main/java/weatherForecastDataProcessing/WeatherForecastDataProcessing.java
@@ -0,0 +1,70 @@
+package weatherForecastDataProcessing;
+
+public class WeatherForecastDataProcessing {
+
+    public static boolean openWeatherForecastFile(){
+        return false;
+    }
+
+    public static String getWeatherForecastLocationLatitude() {
+        return "";
+    }
+
+    public static String getWeatherForecastLocationLongitude() {
+        return "";
+    }
+
+    public static String getWeatherForecastLocationCountry() {
+        return "";
+    }
+
+    public static String getWeatherForecastLocationRegion() {
+        return "";
+    }
+
+    public static String getWeatherForecastLocationTown() {
+        return "";
+    }
+
+    public static String getWeatherForecastDate() {
+        return "";
+    }
+
+    public static String getWeatherForecastTimeStep() {
+        return "";
+    }
+
+    public static String getWeatherForecastTemperatureUnits() {
+        return "";
+    }
+
+    public static void covertToCelsius() {
+    }
+
+    public static void covertToFahrenheit() {
+    }
+
+    public static String getWeatherForecastMaximumTemperature() {
+        return "";
+    }
+
+    public static String getWeatherForecastMinimumTemperature() {
+        return "";
+    }
+
+    public static String getWeatherForecastNebulosity() {
+        return "";
+    }
+
+    public static String getWeatherForecastWindDirection() {
+        return "";
+    }
+
+    public static String getWeatherForecastHumidity() {
+        return "";
+    }
+
+    public static String getWeatherForecastFileExtension() {
+        return "";
+    }
+}
